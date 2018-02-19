af6990cd6ba4c8528a6c31ab4e822743d704cb30
glalex <oleksii.hlaholiev@gmail.com>
2018-02-19 20:03:56 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const bootstrap = require('bootstrap');
 
 module.exports = {
     entry: './src/app.js',
@@ -10,39 +9,31 @@ module.exports = {
     },
     module: {
         rules: [
+            // {
+            //     test: /\.(css|sass|scss)/,
+            //     use: ExtractTextPlugin.extract({
+            //             fallback: 'style-loader',
+            //             use: 'css-loader!sass-loader'
+            //         }
+            //     )
+            // }
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
-            }, {
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
                 test: /\.scss$/,
-                loader: "style-loader!css-loader!sass-loader"
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
             }
-
-            // {
-            //     test: /\.css$/,
-            //     loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            //
-            // },
-            // {
-            //     test: /\.scss$/,
-            //     use: ExtractTextPlugin.extract({
-            //         fallback: 'style-loader',
-            //         use: ['css-loader', 'sass-loader']
-            //     })
-            // }
         ]
-        // loaders: [{
-        //     test: /\.css$/,
-        //     loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-        // }, {
-        //     test: /\.scss$/,
-        //     loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-        // }]
     },
     plugins: [
-        // new ExtractTextPlugin('style.css')
-        // new ExtractTextPlugin("style.css", {
-        //     allChunks: true
-        // })
+        new ExtractTextPlugin('style.css')
     ]
 };
