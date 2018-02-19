6feca08e98650895cdfd4967f4e71a195084eccc
glalex <oleksii.hlaholiev@gmail.com>
2018-02-19 18:34:09 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './scss/base.scss';
+import './scss/main.scss';
+
+import './js/rainbows.js';
+import './js/unicorns.js';
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const bootstrap = require('bootstrap');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+        // loaders: [{
+        //     test: /\.css$/,
+        //     loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        // }, {
+        //     test: /\.scss$/,
+        //     loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+        // }]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+        // new ExtractTextPlugin("style.css", {
+        //     allChunks: true
+        // })
+    ]
+};
