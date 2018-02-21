45ffa6df3e09c3dffa82d0abcc4426947f59f961
glalex <oleksii.hlaholiev@gmail.com>
2018-02-21 10:55:16 +0200

    update

--- src/js/chunks/index.js
+++ src/js/chunks/index.js
@@ -9,7 +9,7 @@ $(function () {
 
         $(coursesSlider).owlCarousel({
             loop:true,
-            margin:15,
+            margin:0,
             items:1,
             nav:false,
             autoplay:false,
